b1e3e5c3dde301731c961d8a83675679d0045f08
Austin Kimla <kimlaa21@hanover.edu>
2020-04-11 22:27:29 -0400

    Forgot to Initial Commit / Completed Task 2 of 01.2

---
+++ app/src/main/java/com/example/android/songdetail/SongDetailActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.songdetail;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.android.songdetail.content.SongUtils;
+
+/**
+ * An activity that shows a fragment with song detail.
+ */
+public class SongDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (savedInstanceState == null) {
+            // Get the selected song position from the intent extra.
+            int selectedSong =
+                    getIntent().getIntExtra(SongUtils.SONG_ID_KEY, 0);
+            // Create instance of the detail fragment and add it to the activity
+            // using a fragment transaction.
+            SongDetailFragment fragment =
+                    SongDetailFragment.newInstance(selectedSong);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.song_detail_container, fragment)
+                    .commit();
+        }
+    }
+
+    /**
+     * Performs action if the user selects the Up button.
+     *
+     * @param item Menu item selected (Up button)
+     * @return True if options menu item is selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown.
+            // NavUtils allows users to navigate up one level in the
+            // application structure.
+            NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
